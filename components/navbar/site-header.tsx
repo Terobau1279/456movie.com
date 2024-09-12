ad5aa6eb5d68d3fab58c876ccf3e47fd77d86171
Terobau1279 <johnwho864@gmail.com>
2024-09-13 01:48:00 +0545

    Update site-header.tsx

--- components/navbar/site-header.tsx
+++ components/navbar/site-header.tsx
@@ -18,40 +18,9 @@ export function SiteHeader() {
         <div className="flex flex-1 items-center space-x-2 justify-end">
           <nav className="flex items-center">
             <CommandSearch />
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
+            
+            
+             
 
             <ThemeToggle />
           </nav>

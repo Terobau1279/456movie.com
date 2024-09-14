568693b91588d43998f31424c61ea07f915a4f6d
Terobau1279 <johnwho864@gmail.com>
2024-09-15 04:50:38 +0545

    Update videoplayer.tsx

--- components/containers/tv/videoplayer.tsx
+++ components/containers/tv/videoplayer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import {
   Select,
@@ -21,7 +22,7 @@ interface Season {
 interface Episode {
   episode_number: number;
   name: string;
-  still_path: string;
+  still_path: string | null; // For episode thumbnail
 }
 
 export default function VideoPlayer({ id }: { id: number }) {
@@ -107,8 +108,8 @@ export default function VideoPlayer({ id }: { id: number }) {
     }
   };
 
-  const handleEpisodeClick = (episodeNumber: string) => {
-    setEpisode(episodeNumber);
+  const handleEpisodeClick = (epNumber: string) => {
+    setEpisode(epNumber);
   };
 
   if (isLoading) {
@@ -130,8 +131,95 @@ export default function VideoPlayer({ id }: { id: number }) {
 
   return (
     <div className="py-8">
-      <div className="max-w-3xl mx-auto px-4">
-        {/* Video Player */}
+      <div className="pb-4">
+        <div className="flex flex-col text-center items-center justify-center">
+          {/* Season and Episode Dropdowns */}
+          <div className="rounded-md pl-4 flex w-full max-w-sm items-center space-x-2">
+            <div className="flex items-center space-x-2">
+              <Select
+                value={season}
+                onValueChange={(e) => setSeason(e)}
+                disabled={isLoading || seasons.length === 0}
+              >
+                <SelectTrigger className="px-4 py-2 rounded-md w-[180px]">
+                  <SelectValue placeholder="Select Season" />
+                </SelectTrigger>
+                <SelectContent>
+                  {seasons.length > 0 ? (
+                    seasons.map((s) => (
+                      <SelectItem
+                        key={s.season_number}
+                        value={s.season_number.toString()}
+                      >
+                        Season {s.season_number}
+                      </SelectItem>
+                    ))
+                  ) : (
+                    <></>
+                  )}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Select
+                value={episode}
+                onValueChange={(e) => setEpisode(e)}
+                disabled={isLoading || episodes.length === 0}
+              >
+                <SelectTrigger className="px-4 py-2 rounded-md w-[180px]">
+                  <SelectValue placeholder="Select Episode" />
+                </SelectTrigger>
+                <SelectContent>
+                  {episodes.length > 0 ? (
+                    episodes.map((s) => (
+                      <SelectItem
+                        key={s.episode_number}
+                        value={s.episode_number.toString()}
+                      >
+                        Episode {s.episode_number}
+                      </SelectItem>
+                    ))
+                  ) : (
+                    <></>
+                  )}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          {/* Download Button */}
+          <div className="pt-2">
+            <Link href={`https://dl.vidsrc.vip/tv/${id}/${season}/${episode}`}>
+              <Badge
+                variant="outline"
+                className="cursor-pointer whitespace-nowrap"
+              >
+                <Download className="mr-1.5" size={12} />
+                Download {season}-{episode}
+              </Badge>
+            </Link>
+          </div>
+          {/* Server Selector */}
+          <div className="pt-4">
+            <Select
+              value={server}
+              onValueChange={(e) => setServer(e)}
+              className="w-[200px]"
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Select Server" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="vidsrccc">VidSrc.cc</SelectItem>
+                <SelectItem value="vidlinkpro">Vidlink.pro</SelectItem>
+                <SelectItem value="autoembed">Autoembed</SelectItem>
+                <SelectItem value="superembed">SuperEmbed</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+      {/* Video Player */}
+      <div className="max-w-3xl mx-auto px-4 pt-10">
         <iframe
           src={getIframeSrc()}
           referrerPolicy="origin"
@@ -140,55 +228,24 @@ export default function VideoPlayer({ id }: { id: number }) {
           height="450"
           scrolling="no"
         ></iframe>
-        <div className="pt-4 text-center">
-          <Link href={`https://dl.vidsrc.vip/tv/${id}/${season}/${episode}`}>
-            <Badge variant="outline" className="cursor-pointer whitespace-nowrap">
-              <Download className="mr-1.5" size={12} />
-              Download {season}-{episode}
-            </Badge>
-          </Link>
-        </div>
-
-        {/* Season Dropdown */}
-        <div className="pt-4 text-center">
-          <Select
-            value={season}
-            onValueChange={(e) => setSeason(e)}
-            disabled={isLoading || seasons.length === 0}
-          >
-            <SelectTrigger className="px-4 py-2 rounded-md w-[180px]">
-              <SelectValue placeholder="Select Season" />
-            </SelectTrigger>
-            <SelectContent>
-              {seasons.map((s) => (
-                <SelectItem key={s.season_number} value={s.season_number.toString()}>
-                  Season {s.season_number}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-
-        {/* Episode Thumbnails */}
-        <div className="pt-8">
-          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
-            {episodes.map((ep) => (
-              <div
-                key={ep.episode_number}
-                className="cursor-pointer"
-                onClick={() => handleEpisodeClick(ep.episode_number.toString())}
-              >
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${ep.still_path}`}
-                  alt={`Episode ${ep.episode_number}`}
-                  className="w-full h-32 object-cover rounded-lg"
-                />
-                <p className="text-center text-sm mt-2">
-                  {ep.episode_number}. {ep.name}
-                </p>
-              </div>
-            ))}
-          </div>
+      </div>
+      {/* Thumbnails Section */}
+      <div className="max-w-5xl mx-auto px-4 pt-10">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          {episodes.map((ep) => (
+            <div
+              key={ep.episode_number}
+              className="cursor-pointer"
+              onClick={() => handleEpisodeClick(ep.episode_number.toString())}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${ep.still_path}`}
+                alt={`Episode ${ep.episode_number}`}
+                className="w-full h-32 object-cover rounded-lg"
+              />
+              <p className="text-center text-sm mt-2">Episode {ep.episode_number}: {ep.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>

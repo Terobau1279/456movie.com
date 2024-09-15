9c21e0a0d8cc8c4a760be5624e9c100c7cda0931
Terobau1279 <johnwho864@gmail.com>
2024-09-15 16:33:43 +0545

    Update videoplayer.tsx

--- components/containers/movie/videoplayer.tsx
+++ components/containers/movie/videoplayer.tsx
@@ -60,9 +60,9 @@ export default function VideoPlayer({ id }: any) {
   };
 
   return (
-    <div className="py-8 mx-auto max-w-5xl">
-      <div className="flex flex-col text-center items-center justify-center">
-        <div className="flex flex-col flex-wrap pb-4">
+    <div className="py-8 mx-auto max-w-6xl px-4">
+      <div className="flex flex-col items-center mb-6">
+        <div className="flex flex-col items-center mb-4">
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem>
@@ -76,46 +76,62 @@ export default function VideoPlayer({ id }: any) {
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
-        </div>
-        <div className="text-xl font-bold mb-4">
-          Currently Watching: <span className="text-blue-500">{movieDetails?.title || id.charAt(0).toUpperCase() + id.slice(1)}</span>
-        </div>
-        {movieDetails && (
-          <div className="text-lg mb-6">
-            <p>{movieDetails?.overview}</p>
+          <div className="text-3xl font-bold mb-4 text-center text-white">
+            Currently Watching: <span className="text-blue-500">{movieDetails?.title || id.charAt(0).toUpperCase() + id.slice(1)}</span>
           </div>
-        )}
+          <div className="relative w-full max-w-3xl mx-auto mb-6">
+            {movieDetails && (
+              <div className="relative rounded-lg overflow-hidden">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`}
+                  alt={movieDetails.title}
+                  className="absolute inset-0 w-full h-full object-cover opacity-60"
+                />
+                <div className="relative z-10 bg-gray-900 bg-opacity-80 p-6 rounded-lg shadow-lg">
+                  <div className="flex flex-col md:flex-row mb-4">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+                      alt={movieDetails.title}
+                      className="w-40 h-60 object-cover rounded-lg shadow-lg mb-4 md:mb-0 md:mr-4"
+                    />
+                    <div className="flex-1">
+                      <h2 className="text-2xl font-bold text-white mb-2">{movieDetails.title}</h2>
+                      <p className="text-gray-300 mb-2">{movieDetails.release_date} • {movieDetails.runtime} mins</p>
+                      <p className="text-gray-400 mb-4">{movieDetails.genres.map((genre: any) => genre.name).join(', ')}</p>
+                      <p className="text-gray-300 mb-4">{movieDetails.overview}</p>
+                      <div className="flex items-center">
+                        <Link href={`https://dl.vidsrc.vip/movie/${id}`}>
+                          <Badge variant="outline" className="cursor-pointer whitespace-nowrap bg-gray-800 text-white border-gray-600">
+                            <Download className="mr-1.5" size={16} />
+                            Download Movie
+                          </Badge>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row items-center justify-center w-full mb-4">
-        <div className="flex flex-col text-center w-full max-w-xs mx-auto">
-          <Select onValueChange={handleSelectChange} value={selectedSource}>
-            <SelectTrigger className="px-4 py-2 rounded-md w-full bg-gray-800 text-white">
-              <SelectValue placeholder="Select Video Source" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="vidlinkpro">Vidlink.pro</SelectItem>
-              <SelectItem value="vidsrccc">VidSrc.cc</SelectItem>
-              <SelectItem value="vidsrcpro">VidSrc.pro</SelectItem>
-              <SelectItem value="superembed">SuperEmbed (Contains Ads)</SelectItem>
-            </SelectContent>
-          </Select>
-          <div className="pt-2 flex justify-center items-center space-x-4">
-            <Link href={`https://dl.vidsrc.vip/movie/${id}`}>
-              <Badge
-                variant="outline"
-                className="cursor-pointer whitespace-nowrap bg-gray-800 text-white border-gray-600"
-              >
-                <Download className="mr-1.5" size={16} />
-                Download Movie
-              </Badge>
-            </Link>
-          </div>
-        </div>
+      <div className="flex flex-row items-center justify-center mb-6">
+        <Select onValueChange={handleSelectChange} value={selectedSource} className="w-full max-w-xs">
+          <SelectTrigger className="px-4 py-2 rounded-md w-full bg-gray-800 text-white">
+            <SelectValue placeholder="Select Video Source" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="vidlinkpro">Vidlink.pro</SelectItem>
+            <SelectItem value="vidsrccc">VidSrc.cc</SelectItem>
+            <SelectItem value="vidsrcpro">VidSrc.pro</SelectItem>
+            <SelectItem value="superembed">SuperEmbed (Contains Ads)</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
       {loading ? (
         <Skeleton className="mx-auto px-4 pt-6 w-full h-[500px]" />
       ) : (
-        <div className="relative w-full max-w-3xl mx-auto px-4 pt-6">
+        <div className="relative w-full max-w-3xl mx-auto px-4 pt-6 mb-6">
           <iframe
             src={videoSources[selectedSource]}
             referrerPolicy="origin"
@@ -129,17 +145,17 @@ export default function VideoPlayer({ id }: any) {
       )}
       {movieDetails && (
         <div className="pt-6">
-          <h2 className="text-2xl font-bold mb-4">Cast</h2>
+          <h2 className="text-2xl font-bold mb-4 text-white">Cast</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
             {movieCast.map((castMember: any) => (
-              <div key={castMember.id} className="flex flex-col items-center">
+              <div key={castMember.id} className="flex flex-col items-center text-center">
                 <img
                   src={`https://image.tmdb.org/t/p/w200${castMember.profile_path}`}
                   alt={castMember.name}
-                  className="rounded-full mb-2 w-24 h-24 object-cover"
+                  className="rounded-full mb-2 w-24 h-24 object-cover border-2 border-gray-800"
                 />
-                <span className="text-center text-sm font-medium">{castMember.name}</span>
-                <span className="text-center text-xs text-gray-500">{castMember.character}</span>
+                <span className="text-sm font-medium text-white">{castMember.name}</span>
+                <span className="text-xs text-gray-400">{castMember.character}</span>
               </div>
             ))}
           </div>

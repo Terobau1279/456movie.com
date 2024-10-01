247790ee876e19cf2b8ce64148d58015a63fad74
Terobau1279 <johnwho864@gmail.com>
2024-10-02 00:54:45 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default async function Home() {
   return (
     <>
       <Pattern variant="checkered" />
-      <div className="mx-auto max-w-4xl p-4 bg-black bg-opacity-70">
+      <div className="mx-auto max-w-4xl p-4 bg-gradient-to-b from-gray-900 via-gray-800 to-gray-700">
         <section className="flex h-[75vh] items-center md:h-[50vh]">
           <div className="mx-auto flex w-4/5 flex-col items-center justify-center space-y-6 text-center">
-            <h1 className="text-6xl font-bold text-white">
+            <h1 className="text-6xl font-bold text-white drop-shadow-md">
               Explore Movies, TV Series, and Animes!
             </h1>
-            <p className="text-lg leading-7 text-gray-300">
+            <p className="text-lg leading-7 text-gray-200 drop-shadow-md">
               456movie is a streaming platform for those who love to relax
               and watch millions of movies, series, and animes for free. 
               <br />

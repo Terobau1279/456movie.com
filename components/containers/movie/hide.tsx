45ce28ca4d996c0c664eaad64f744ef5e2d973f7
Terobau1279 <johnwho864@gmail.com>
2024-10-01 22:55:59 +0545

    Update hide.tsx

--- components/containers/movie/hide.tsx
+++ components/containers/movie/hide.tsx
@@ -1,1 +1,20 @@
+"use client";
+import { useState } from "react";
 
+type HideProps = {
+  toggleRelatedMovies: () => void;
+  showRelatedMovies: boolean;
+};
+
+export default function Hide({ toggleRelatedMovies, showRelatedMovies }: HideProps) {
+  return (
+    <div className="my-8">
+      <button
+        onClick={toggleRelatedMovies}
+        className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition duration-300"
+      >
+        {showRelatedMovies ? "Hide Related Movies" : "Show Related Movies"}
+      </button>
+    </div>
+  );
+}

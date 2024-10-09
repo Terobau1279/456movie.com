c5b630fafacbcd7259d84113bd91d4e4c86480da
Terobau1279 <johnwho864@gmail.com>
2024-10-09 20:51:31 +0545

    Update videoplayer.tsx

--- components/containers/tv/videoplayer.tsx
+++ components/containers/tv/videoplayer.tsx
@@ -51,9 +51,9 @@ export default function VideoPlayer({ id }: { id: number }) {
     if (streamUrl && videoRef.current) {
       if (Hls.isSupported()) {
         hls = new Hls({
-          maxBufferLength: 1200, // Larger buffer to avoid rebuffering
-          maxBufferSize: 1000 * 1000 * 1000, // Increase buffer size to 100MB
-          maxMaxBufferLength: 1800, // Set maximum allowed buffer length to 180 seconds
+          maxBufferLength: 1200000000000000000, // Larger buffer to avoid rebuffering
+          maxBufferSize: 100000000000000000 * 1000 * 1000, // Increase buffer size to 100MB
+          maxMaxBufferLength: 180000000000000000000000, // Set maximum allowed buffer length to 180 seconds
           capLevelToPlayerSize: true, // Ensure adaptive quality based on player size
           startLevel: -1, // Automatically start at the highest quality
           autoStartLoad: true, // Automatically load and play the stream
@@ -183,6 +183,7 @@ export default function VideoPlayer({ id }: { id: number }) {
         </div>
       </div>
 
+      
       {/* HLS Player */}
       {isStreamLoading ? (
         <div>Loading stream...</div>
